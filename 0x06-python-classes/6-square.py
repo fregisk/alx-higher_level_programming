69ef4fca77e2544564e9b665d2f40a8b4c2f4858
fregisk <fregiskonan34@gmail.com>
2022-10-11 22:34:34 +0000

    tasks

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -39,3 +39,24 @@ class Square:
     def size(self, value):
         """Setter for the size private attribute"""
         if (type(value) is not int):
+            raise (TypeError("size must be an integer"))
+        elif (value < 0):
+            raise (ValueError("size must be >= 0"))
+        else:
+            self.__size = value
+
+    @property
+    def position(self):
+        """Getter of Position"""
+        return (self.__position)
+
+    @position.setter
+    def position(self, value):
+        """Setter of position"""
+        if (len(value) != 2) or (type(value) is not tuple) \
+                or (type(value[0]) is not int) \
+                or (type(value[1]) is not int) \
+                or (value[0] < 0) or (value[1] < 0):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
