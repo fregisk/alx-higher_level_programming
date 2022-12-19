225f48fc70a982b4df76250db34268b1352aa318
fregisk <fregiskonan34@gmail.com>
2022-12-19 07:35:33 -0800

    tasks git commit -m tasks

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const array = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (let i = 0; i < array.length; i++) {
-  console.log(array[i]);
+const argv = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (const arg in argv) {
+  console.log(argv[arg]);
 }
